2c8f196359b8a039c7ebb3f7c9e2c9db8eefb4f6
SrDoppelganger <doppelganger.dev2004@gmail.com>
2025-02-13 18:32:28 -0300

    Added gato and basic movement

---
+++ playerScript.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+
+	
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var horizontal_direction = Input.get_axis("left", "right")
+	if horizontal_direction:
+		velocity.x = horizontal_direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	var vertical_direction = Input.get_axis("up", "down")
+	if vertical_direction:
+		velocity.y = vertical_direction * SPEED
+	else:
+		velocity.y = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
