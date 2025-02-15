82bc72c6973dee9fa5224f5326c9906ac345f5fd
SrDoppelganger <doppelganger.dev2004@gmail.com>
2025-02-15 16:15:37 -0300

    made the gun point at the cursor
    Added some scripts to make the gun point at the global mouse position

--- scripts/gunScript.gd
+++ scripts/gunScript.gd
@@ -1,11 +1,14 @@
 extends Node2D
 
+var mousePos
+@onready var gun_slot = $".."
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	#pega as coordenadas do mouse na tela
+	mousePos = get_global_mouse_position();
+	gun_slot.look_at(mousePos);
