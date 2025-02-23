bd7bb3c7ab842a58b9c96be5bd3219518f895585
SrDoppelganger <doppelganger.dev2004@gmail.com>
2025-02-23 07:39:49 -0300

    Implemented Killzone
    now enemies can actually kill the player

--- scripts/bulletScript.gd
+++ scripts/bulletScript.gd
@@ -11,3 +11,9 @@ func _process(delta):
 #talvez precise retrabalhar essa lógica a depender do design do jogo
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free();
+
+#mata os inimigos e tira a bala da cena
+func _on_body_entered(body):
+	if body.is_in_group("mobs"):
+		body.queue_free();
+	queue_free();
---
+++ scripts/enemyScript.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+
+@onready var player = %player;
+const SPEED = 75.0;
+var direction;
+
+func getPlayerPosition():
+	return player.position - position
+
+func _physics_process(delta):
+	var direction = getPlayerPosition();
+	velocity = direction * SPEED * delta;
+	
+	move_and_slide();
+	
