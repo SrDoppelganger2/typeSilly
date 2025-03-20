65e8181966a3f0079b7dad606f383a5b19b8a5f4
SrDoppelganger <doppelganger.dev2004@gmail.com>
2025-03-20 16:44:02 -0300

    Started working on options menu

--- scripts/titleScript.gd
+++ scripts/titleScript.gd
@@ -5,6 +5,10 @@ extends Node2D
 func _on_jogar_pressed():
 	get_tree().change_scene_to_file("res://scenes/main_scene.tscn");
 
+func _on_config_pressed():
+	get_tree().change_scene_to_file("res://scenes/options.tscn");
 
 func _on_sair_pressed():
 	get_tree().quit();
+
+
