7f7d2dd4998a86662081cb870c620a9a86997860
srdoppelganger <arthur.albuquerque2004@gmail.com>
2025-05-13 12:01:35 -0300

    added some simple animations to selection screen

--- scripts/selectionScript.gd
+++ scripts/selectionScript.gd
@@ -2,13 +2,29 @@ extends Control
 
 var weapon;
 var weaponPicked:bool = false
-@onready var jogarButton = $VBoxContainer/Jogar
+@onready var jogarButton = %Jogar;
+@onready var melancio = $melancio;
+@onready var janela = $janela;
+
+func _ready() -> void:
+	melancioAnimation();
+	janelaAnimation();
 
 func _process(_delta: float) -> void:
 	if !weaponPicked:
 		jogarButton.disabled = true;
 	else:
 		jogarButton.disabled = false;
+
+func melancioAnimation():
+	var panelAnimation = melancio.create_tween();
+	panelAnimation.tween_property(melancio,"position",Vector2(-68,138),0.4).set_trans(panelAnimation.TRANS_EXPO).set_ease(panelAnimation.EASE_IN_OUT);
+	panelAnimation.play();
+
+func janelaAnimation():
+	var panelAnimation = janela.create_tween();
+	panelAnimation.tween_property(janela,"position",Vector2(520,90),0.4).set_trans(panelAnimation.TRANS_EXPO).set_ease(panelAnimation.EASE_IN_OUT);
+	panelAnimation.play();
 
 func _on_jogar_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main_scene.tscn");
