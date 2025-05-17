5b97a7e0e2716c82b8728db15d1a5f3b745a0d7d
srdoppelganger <arthur.albuquerque2004@gmail.com>
2025-05-17 18:08:09 -0300

    started adding definitive graphics

--- scripts/options.gd
+++ scripts/options.gd
@@ -3,3 +3,17 @@ extends Node2D
 
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/title.tscn");
+
+#configurações de audio
+func _on_masterVolume_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(0,linear_to_db(value));
+	
+func _on_mutar_master_toggled(toggled_on: bool) -> void:
+	AudioServer.set_bus_mute(0,toggled_on)
+	
+
+func _on_musicVolume_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(1,linear_to_db(value));
+	
+func _on_mutar_musica_toggled(toggled_on: bool) -> void:
+	AudioServer.set_bus_mute(1,toggled_on);
