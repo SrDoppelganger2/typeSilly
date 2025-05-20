66fa787cb1374fc2585fd6b1c50c8b40572a12de
srdoppelganger <arthur.albuquerque2004@gmail.com>
2025-05-19 22:09:44 -0300

    finished first release version

--- scripts/prologo.gd
+++ scripts/prologo.gd
@@ -2,6 +2,7 @@ extends Control
 
 @onready var music: AudioStreamPlayer = $music
 @onready var intro: VideoStreamPlayer = $intro
+@onready var aviso: Label = $aviso
 
 
 func _ready() -> void:
@@ -16,3 +17,7 @@ func _input(event: InputEvent) -> void:
 	if event is InputEventKey:
 		Soundtrack.playEffect("accept");
 		get_tree().change_scene_to_file("res://scenes/title.tscn");
+
+
+func _on_timer_timeout() -> void:
+	aviso.visible = false;
